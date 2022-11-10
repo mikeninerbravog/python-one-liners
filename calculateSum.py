3a14814b7d4472f65add945f93044aeea3df44e0
mikeninerbravog <bravog.bot2@gmail.com>
2022-11-10 08:21:58 -0300

    uploading

--- calculateSum.py
+++ calculateSum.py
@@ -5,3 +5,25 @@ list3 = [0, 43, 35, 12, 45]
 print(sum(list1))
 print(sum(list2))
 print(sum(list3))
+
+"""
+This snippet of Python code defines three lists, `list1`, `list2`, and `list3`, and then 
+calculates the sum of the elements in each of these lists using the `sum()` function. 
+
+Here's a breakdown of the code:
+
+1. `list1 = [1, 2, 3, 4, 5, 6, 7]`: This line defines a list called `list1` that contains seven integers: 1, 2, 3, 4, 5, 6, and 7.
+
+2. `list2 = [324, 435, 456]`: This line defines another list called `list2` that contains three integers: 324, 435, and 456.
+
+3. `list3 = [0, 43, 35, 12, 45]`: This line defines a third list called `list3` with five integers: 0, 43, 35, 12, and 45.
+
+4. `print(sum(list1))`: This line calculates the sum of all the elements in `list1` using the `sum()` function and then prints the result. In this case, it adds up 1 + 2 + 3 + 4 + 5 + 6 + 7, which equals 28, so it will print `28`.
+
+5. `print(sum(list2))`: This line calculates the sum of all the elements in `list2` (324 + 435 + 456) and prints the result. The sum is 1215, so it will print `1215`.
+
+6. `print(sum(list3))`: Similarly, this line calculates the sum of all the elements in `list3` (0 + 43 + 35 + 12 + 45) and prints the result. The sum is 135, so it will print `135`.
+
+So, the code essentially computes and displays the sum of the elements in each of the three lists.
+
+"""
