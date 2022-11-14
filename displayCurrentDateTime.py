4f5683c92b84ddbee88a30fd0a20ba21570f6086
mikeninerbravog <bravog.bot2@gmail.com>
2022-11-14 08:44:08 -0300

    uploading

--- displayCurrentDateTime.py
+++ displayCurrentDateTime.py
@@ -1,1 +1,30 @@
 import datetime; print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+
+"""
+This code snippet uses the `datetime` module in Python to print the current date and time in a specific format. Here's a breakdown of the code:
+
+1. `import datetime`: This line imports the `datetime` module, which provides classes for working with dates and times in Python.
+
+2. `datetime.datetime.now()`: This part of the code calls the `now()` method of the `datetime` module, 
+which returns the current date and time as a `datetime` object.
+
+3. `.strftime("%Y-%m-%d %H:%M:%S")`: The `strftime()` method is called on the `datetime` object. 
+
+It allows you to format the date and time as a string according to a specified format. In this case, the format is `"%Y-%m-%d %H:%M:%S"`, which stands for:
+   - `%Y`: Year with century as a decimal number (e.g., 2023).
+   - `%m`: Month as a zero-padded decimal number (e.g., 01 for January).
+   - `%d`: Day of the month as a zero-padded decimal number (e.g., 07 for the 7th day).
+   - `%H`: Hour (24-hour clock) as a zero-padded decimal number (e.g., 14 for 2:00 PM).
+   - `%M`: Minute as a zero-padded decimal number (e.g., 05).
+   - `%S`: Second as a zero-padded decimal number (e.g., 09).
+
+4. `print(...)`: Finally, the formatted date and time string is printed to the console.
+
+The output of this code will be the current date and time in the specified format, for example:
+
+```
+2023-11-08 14:05:09
+```
+
+The exact date and time will vary depending on when you run the code.
+"""
