dca60f42cd47ba3f4c166d63d9d07ff809ff1fba
mikeninerbravog <bravog.bot2@gmail.com>
2022-11-17 09:30:58 -0300

    uploading

--- fizzBuzz.py
+++ fizzBuzz.py
@@ -1,1 +1,45 @@
 for i in range(1, 21): print("Fizz"*(i%3==0)+"Buzz"*(i%5==0) or str(i))
+
+"""
+This code is a concise way to print the numbers from 1 to 20, replacing numbers divisible by 3 with "Fizz" and numbers divisible by 5 with "Buzz" 
+following the rules of the FizzBuzz game. Here's how it works:
+
+1. `for i in range(1, 21)`: This is a `for` loop that iterates over the numbers from 1 to 20 (inclusive).
+
+2. `print("Fizz"*(i%3==0)+"Buzz"*(i%5==0) or str(i))`: This line prints the appropriate output for each number `i` based on the FizzBuzz rules.
+
+   - `"Fizz"*(i%3==0)`: This part of the line checks if `i` is divisible by 3 (i.e., if `i % 3` equals 0). If it is divisible by 3, it produces 
+   the string "Fizz"; otherwise, it produces an empty string.
+   - `"Buzz"*(i%5==0)`: This part checks if `i` is divisible by 5 (i.e., if `i % 5` equals 0). If it is divisible by 5, it produces the string "Buzz"; 
+   otherwise, it produces an empty string.
+   - `or str(i)`: If both of the above conditions are not met (i.e., if `i` is neither divisible by 3 nor by 5), it converts the number `i` 
+   to a string using `str(i)`.
+
+So, the line combines "Fizz" and "Buzz" if the number is divisible by 3 and 5 (for example, "FizzBuzz" for 15), and it prints the number itself 
+as a string for all other cases.
+
+The output of this code will be the FizzBuzz sequence for the numbers from 1 to 20:
+
+```
+1
+2
+Fizz
+4
+Buzz
+Fizz
+7
+8
+Fizz
+Buzz
+11
+Fizz
+13
+14
+FizzBuzz
+16
+17
+Fizz
+19
+Buzz
+```
+"""
