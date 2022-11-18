d89fb236d0b415cd461cea6e2864f465faea152f
mikeninerbravog <bravog.bot2@gmail.com>
2022-11-18 09:35:01 -0300

    uploading

--- generatePassword.py
+++ generatePassword.py
@@ -1,1 +1,27 @@
 import random as r; p = 'abcdefghijklmnopqrstuvwxyz0123456789%^*(-_=+)'; print(''.join(r.choices(p, k=10)))
+
+"""
+This code generates a random password consisting of lowercase letters, numbers, and some special characters. Here's a breakdown of the code:
+
+1. `import random as r`: This line imports the `random` module and aliases it as `r`, making it easier to refer to the module functions.
+
+2. `p = 'abcdefghijklmnopqrstuvwxyz0123456789%^*(-_=+)'`: This line defines a string `p` containing the characters from which the password will be generated. It includes lowercase letters, numbers, and a set of special characters.
+
+3. `r.choices(p, k=10)`: This part of the code uses the `choices()` function from the `random` module to select 10 random characters from the string `p`. 
+The `k` parameter specifies the number of characters to select.
+
+4. `''.join(...)`: This line joins the list of randomly selected characters into a single string. It uses an empty string as the separator.
+
+5. `print(...)`: Finally, the generated password is printed to the console.
+
+When you run this code, it will produce a random 10-character password that can contain a combination of lowercase letters, numbers, 
+and the special characters specified in the string `p`.
+
+Here's an example of the kind of output you might get:
+
+```
+a3*5+z7_8b
+```
+
+The specific password will vary each time you run the code due to its random nature.
+"""
